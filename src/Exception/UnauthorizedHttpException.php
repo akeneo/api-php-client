8370f1ba79748a0d06d9ae5c898ecc3503fe4513
Alexandre Hocquard <alexandre.hocquard@akeneo.com>
2017-04-24 09:52:29 +0200

    API-145: add http exceptions handling

---
+++ src/Exception/UnauthorizedHttpException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Akeneo\Pim\Exception;
+
+/**
+ * Exception thrown when it is an unauthorized request exception (401).
+ *
+ * @author    Alexandre Hocquard <alexandre.hocquard@akeneo.com>
+ * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class UnauthorizedHttpException extends ClientErrorHttpException
+{
+}
