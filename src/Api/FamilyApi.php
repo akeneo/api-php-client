366bffb8d48f8de0de570c7f2297b23009e3c979
Laurent Petard <laurent.petard@akeneo.com>
2017-05-24 12:16:53 +0200

    API-236: Implements API to get a single family

--- src/Api/FamilyApi.php
+++ src/Api/FamilyApi.php
@@ -16,6 +16,7 @@ use Akeneo\Pim\Pagination\ResourceCursorFactoryInterface;
 class FamilyApi implements FamilyApiInterface
 {
     const FAMILIES_PATH = 'api/rest/v1/families';
+    const FAMILY_PATH = 'api/rest/v1/families/%s';
 
     /** @var ResourceClientInterface */
     protected $resourceClient;
@@ -39,6 +40,14 @@ class FamilyApi implements FamilyApiInterface
         $this->resourceClient = $resourceClient;
         $this->pageFactory = $pageFactory;
         $this->cursorFactory = $cursorFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get($code)
+    {
+        return $this->resourceClient->getResource(static::FAMILY_PATH, [$code]);
     }
 
     /**
