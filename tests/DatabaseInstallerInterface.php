eda45de3c88d707d1b44fde63fa576661e131819
Laurent Petard <laurent.petard@akeneo.com>
2017-06-08 09:57:27 +0200

    Initialize integration tests

---
+++ tests/DatabaseInstallerInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Akeneo\Pim\tests;
+
+/**
+ * Aims to install fixtures into the PIM in order to reset the database between the test.
+ *
+ * @author    Alexandre Hocquard <alexandre.hocquard@akeneo.com>
+ * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+interface DatabaseInstallerInterface
+{
+    /**
+     * Install the database of the PIM.
+     *
+     * @param string $path    path to Akeneo PIM application
+     * @param array  $options additional options to install the PIM
+     *
+     * @throws \RuntimeException if an error occured during the PIM install
+     */
+    public function install($path, array $options = []);
+}
