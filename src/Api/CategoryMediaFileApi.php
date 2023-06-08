c35d9a84875431dc212543aa3bd3a44794c42ca5
jeremy-beaucousin <jeremy.beaucousin@akeneo.com>
2023-06-08 19:36:37 +0200

    GRF-862 : Add api/rest/v1/category-media-files/%s/download route to the client

---
+++ src/Api/CategoryMediaFileApi.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Akeneo\Pim\ApiClient\Api;
+
+use Akeneo\Pim\ApiClient\Api\Operation\DownloadableResourceInterface;
+use Akeneo\Pim\ApiClient\Client\ResourceClientInterface;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * API implementation to manage the media files for the products.
+ *
+ * @author    Laurent Petard <laurent.petard@akeneo.com>
+ * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class CategoryMediaFileApi implements DownloadableResourceInterface
+{
+    public const MEDIA_FILE_DOWNLOAD_URI = 'api/rest/v1/category-media-files/%s/download';
+
+    public function __construct(
+        protected ResourceClientInterface $resourceClient,
+    ) {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function download(string $code): ResponseInterface
+    {
+        return $this->resourceClient->getStreamedResource(static::MEDIA_FILE_DOWNLOAD_URI, [$code]);
+    }
+}
